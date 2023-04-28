86797db51c397025eb57472fa079928b2d61b961
Selena Y. Zhou <f006h51@plank.thayer.dartmouth.edu>
2023-04-27 22:09:17 -0400

    implemented pseudocode, no testing

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -1,5 +1,8 @@
 /*
  * crawler.c
+ * exit(1): incorrect number of args, caught in main
+ * exit(2): incorrect input, caught in parseArgs
+ * exit(3): incorrect memory allocation, caught in crawler
  *
  * Selena Zhou, CS50 23S
  *
@@ -10,6 +13,9 @@
 #include <string.h>
 #include "webpage.h"
 #include "pagedir.h"
+#include "bag.h"
+#include "hashtable.h"
+#include "set.h"
 
 static void parseArgs(const int argc, char* argv[], char** seedURL, char** pageDirectory, int* maxDepth);
 static void crawl(char* seedURL, char* pageDirectory, const int maxDepth);
@@ -23,6 +29,7 @@ int main(const int argc, char* argv[]) {
         exit(1);
     }
 
+    // Calls parseArgs and crawl
     char** seedURL; char** pageDirectory; int* maxDepth;
     parseArgs(argc, argv, seedURL, pageDirectory, maxDepth); // will exit if not correct
     printf("Crawling %s with depth %d...\n", seedURL, maxDepth);
@@ -54,32 +61,60 @@ static void parseArgs(const int argc, char* argv[], char** seedURL, char** pageD
 
 }
 
+static void crawl(char* seedURL, char* pageDirectory, const int maxDepth) {
+
+    // initialize the hashtable and add the seedURL
+    // initialize the bag and add a webpage representing the seedURL at depth 0
+    webpage_t* seedPage = webpage_new(seedURL, 0, NULL);
+    bag_t* crawlBag = bag_new();
+    hashtable_t seenURLs = hashtable_new(100*(maxDepth+1));   // upper limit of 100 per page
+    if (seedPage == NULL || crawlBag == NULL || seenURLs == NULL) {
+        fprintf(stderr, "Failed to initialize bag or hashtable\n");
+        exit(3);
+    }
+    bag_insert(crawlBag, seedPage);
+    hashtable_insert(seenURLS, seedURL, seedPage);
+
+    // while bag is not empty, pull a webpage from the bag
+    webpage_t* currentPage;
+    int id = 1;
+    while ((currentPage = bag_extract(crawlBag)) != NULL) {
+        // fetch the HTML for that webpage. if fetch was successful, save the webpage to pageDirectory
+        if (webpage_fetch(currentPage) != NULL) {
+            pagedir_save(currentPage, pageDirectory, id);
+            //if the webpage is not at maxDepth, pageScan that HTML
+            if (webpage_getDepth(currentPage) != maxDepth) {
+                pageScan(currentPage, crawlBag, seenURLs);
+            }
+        }
+        // delete that webpage
+        webpage_delete(currentPage);
+    }
+
+    // delete the hashtable, delete the bag
+    hashtable_delete(seenURLs);
+    bag_delete(crawlBag);
+
+}
+
 static void pageScan(webpage_t* page, bag_t* pagesToCrawl, hashtable_t* pagesSeen) {
 
     int pos = 0;
     char* nextpageURL;
+    // while there is another URL in the page
     while((nextpageURL = webpage_getNextURL(page, &pos)) != NULL) {
-
+        // if that URL is Internal, insert the webpage into the hashtable
+        // if that succeeded, create a webpage_t for it, insert the webpage into the bag
         if (isInternalURL(nextPageURL)) {
             if (hashtable_insert(pagesSeen, nextPageURL, "")) {
                 webpage_t* nextPage = webpage_new(nextPageURL, webpage_getDepth(page)+1, NULL);
-                printf("Webpage inserted")
+                bag_insert(pagesToCrawl, nextPage);
+                printf("Webpage inserted");
             }
         }
+        // free the URL
+        free(nextpageURL);
 
     }
-
-    /*
-    pageScan
-This function implements the pagescanner mentioned in the design. Given a webpage, scan the given page to extract any links (URLs),
-ignoring non-internal URLs; for any URL not already seen before (i.e., not in the hashtable), 
-add the URL to both the hashtable pages_seen and to the bag pages_to_crawl. Pseudocode:
-
-while there is another URL in the page
-	if that URL is Internal,
-		insert the webpage into the hashtable
-		if that succeeded,
-			create a webpage_t for it
-			insert the webpage into the bag
-	free the URL*/
+    
 }
